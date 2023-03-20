050da86d84d0eb0967baaa0ea1a4dc03d3a40ce2
Hesam <hesam.hb.bagherpour@gmail.com>
2023-03-20 12:26:43 +0330

    [add] create clickableObjectSystem

---
+++ Assets/Scripts/HB/Utilities/ClickableObject/ClickableObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace HB.Utilities.ClickableObject
+{
+    public class ClickableObject : MonoBehaviour
+    {
+        public bool is3D = false;
+        public string[] maskLayers = null;
+        private bool _isDown = false;
+        public event Action Pressed = null;
+        public event Action Released = null;
+        public event Action Clicked = null;
+        protected virtual void Awake()
+        {
+            if (ClickableObjectSystem.Instance != null) 
+                ClickableObjectSystem.Instance.AddClickableObject(this);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            ClickableObjectSystem.Instance?.RemoveClickableObject(this);
+        }
+        internal void CallMousePress()
+        {
+            _isDown = true;
+            Pressed?.Invoke();
+        }
+        internal void CallMouseRelease()
+        {
+            bool isClick = _isDown;
+            if (_isDown)
+            {
+                _isDown = false;
+                Released?.Invoke();
+            }
+
+            if (isClick)
+            {
+                Clicked?.Invoke();
+            }
+        }
+        internal void HandleMouseRelease()  //Called When mouse release in happened but mouse is not over this object
+        {
+            if (_isDown)
+            {
+                _isDown = false;
+                Released?.Invoke();
+            }
+        }
+    }
+}
